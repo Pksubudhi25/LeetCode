b13c34dcf0f5ed66261e357898628c978d737977
Pksubudhi25 <97595529+Pksubudhi25@users.noreply.github.com>
2024-06-04 16:01:19 +0530

    Time: 2 ms (52.83%) | Memory: 7.2 MB (55.54%) - LeetSync

---
+++ BinarySearch/BS_On_2D_Matrix/1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  string defangIPaddr(string address) {
+    string defangedAddress;
+    for (int i = 0; i < address.size(); i++) {
+      if (address[i] == '.') {
+        defangedAddress += "[.]";
+      } else {
+        defangedAddress += address[i];
+      }
+    }
+    return defangedAddress;
+  }
+};
